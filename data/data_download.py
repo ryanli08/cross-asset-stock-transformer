572d1cd59ff6253ad9643eef38791b49811f491d
Tan Phan <tanphan@tans-air.myfiosgateway.com>
2025-12-05 16:28:39 -0500

    Add dataset builder

--- data/data_download.py
+++ data/data_download.py
@@ -31,28 +31,446 @@ default_tickers = [
     "LLY.US",     
     "AVGO.US",    
 ]
+# 100 stocks 
+# default_tickers = [
+# "NVDA.US",  # Electronic Technology
+# "AAPL.US",  # Electronic Technology
+# "AVGO.US",  # Electronic Technology
+# "AMD.US",   # Electronic Technology
+# "GE.US",    # Electronic Technology
+# "CSCO.US",  # Electronic Technology
+# "MU.US",    # Electronic Technology
+# "RTX.US",   # Electronic Technology
+# "INTC.US",  # Electronic Technology
+# "QCOM.US",  # Electronic Technology
+# "APH.US",   # Electronic Technology
+# "AMAT.US",  # Electronic Technology
+# "LRCX.US",  # Electronic Technology
+# "TXN.US",   # Electronic Technology
+# "KLAC.US",  # Electronic Technology
+# "BA.US",    # Electronic Technology
+# "ARM.US",   # Electronic Technology
+# "ADI.US",   # Electronic Technology
+# "HON.US",   # Electronic Technology
+# "LMT.US",   # Electronic Technology
+# "DELL.US",  # Electronic Technology
+# "GD.US",  # Electronic Technology
+# "MRVL.US",  # Electronic Technology
+# "HWM.US",  # Electronic Technology
+# "NOC.US",  # Electronic Technology
+# "GOOG.US", # Technology Services
+# "MSFT.US", # Technology Services
+# "META.US", # Technology Services
+# "ORCL.US", # Technology Services
+# "NFLX.US", # Technology Services
+# "PLTR.US", # Technology Services
+# "IBM.US",  # Technology Services
+# "CRM.US",  # Technology Services
+# "APP.US",  # Technology Services
+# "INTU.US", # Technology Services
+# "NOW.US",  # Technology Services
+# "ACN.US",  # Technology Services
+# "ADBE.US", # Technology Services
+# "PANW.US", # Technology Services
+# "CRWD.US", # Technology Services
+# "SNOW.US", # Technology Services
+# "ADP.US",  # Technology Services
+# "NTES.US",  # Technology Services
+# "CDNS.US", # Technology Services
+# "SNPS.US", # Technology Services
+# "NET.US", # Technology Services
+# "ADSK.US", # Technology Services
+# "RBLX.US", # Technology Services
+# "FTNT.US", # Technology Services
+# "WDAY.US", # Technology Services
+# "JPM.US",   # Finance
+# "V.US",     # Finance
+# "MA.US",    # Finance
+# "BAC.US",   # Finance
+# "MS.US",    # Finance
+# "WFC.US",   # Finance
+# "AXP.US",   # Finance
+# "GS.US",    # Finance
+# "C.US",     # Finance
+# "BX.US",    # Finance
+# "BLK.US",   # Finance
+# "SCHW.US",  # Finance
+# "COF.US",   # Finance
+# "WELL.US",   # Finance
+# "PGR.US",   # Finance
+# "PLD.US",   # Finance
+# "CB.US",   # Finance
+# "BN.US",  # Finance
+# "HOOD.US",   # Finance
+# "KKR.US",    # Finance
+# "IBKR.US",   # Finance
+# "CME.US",   # Finance
+# "MMC.US",  # Finance
+# "ICE.US",    # Finance
+# "NU.US",   # Finance
+# "AMZN.US",  # Retail Trade
+# "WMT.US",   # Retail Trade
+# "COST.US",  # Retail Trade
+# "BABA.US",  # Retail Trade
+# "HD.US",    # Retail Trade
+# "TJX.US",   # Retail Trade
+# "PDD.US",  # Retail Trade
+# "LOW.US",   # Retail Trade
+# "MELI.US",  # Retail Trade
+# "CVS.US",  # Retail Trade
+# "ORLY.US",  # Retail Trade
+# "CVNA.US",   # Retail Trade
+# "SE.US",   # Retail Trade
+# "AZO.US",  # Retail Trade
+# "ROST.US",  # Retail Trade
+# "CPNG.US",  # Retail Trade
+# "KR.US",  # Retail Trade
+# "TGT.US",  # Retail Trade
+# "JD.US",  # Retail Trade
+# "CPRT.US",  # Retail Trade
+# "EBAY.US",  # Retail Trade
+# "TSCO.US",  # Retail Trade
+# "ULTA.US",  # Retail Trade
+# "DG.US",  # Retail Trade
+# "TPR.US",  # Retail Trade
+# "XLK.US",     
+# "XLE.US",     
+# "XLY.US", 
+# ]
 
-all_tickers = default_tickers + [
-    "CRM.US",     
-    "ORCL.US",    
-    "INTC.US",    
-    "AMD.US",     
-    "NFLX.US",
-    "BAC.US",     
-    "WFC.US",     
-    "GS.US",      
-    "MS.US",      
-    "UNH.US",
-    "PFE.US",     
-    "TMO.US",
-    "KO.US",      
-    "WMT.US",     
-    "DIS.US",     
-    "NKE.US",     
-    "BA.US",      
-    "CAT.US",     
-    "GE.US",      
-]
+# 305 stocks
+# default_tickers = [
+# "NVDA.US",  # Electronic Technology
+# "AAPL.US",  # Electronic Technology
+# "AVGO.US",  # Electronic Technology
+# "AMD.US",   # Electronic Technology
+# "GE.US",    # Electronic Technology
+# "CSCO.US",  # Electronic Technology
+# "MU.US",    # Electronic Technology
+# "RTX.US",   # Electronic Technology
+# "INTC.US",  # Electronic Technology
+# "QCOM.US",  # Electronic Technology
+# "APH.US",   # Electronic Technology
+# "AMAT.US",  # Electronic Technology
+# "LRCX.US",  # Electronic Technology
+# "TXN.US",   # Electronic Technology
+# "KLAC.US",  # Electronic Technology
+# "BA.US",    # Electronic Technology
+# "ARM.US",   # Electronic Technology
+# "ADI.US",   # Electronic Technology
+# "HON.US",   # Electronic Technology
+# "LMT.US",   # Electronic Technology
+# "DELL.US",  # Electronic Technology
+# "GD.US",  # Electronic Technology
+# "MRVL.US",  # Electronic Technology
+# "HWM.US",  # Electronic Technology
+# "NOC.US",  # Electronic Technology
+# "TDG.US",  # Electronic Technology
+# "EMR.US",  # Electronic Technology
+# "GLW.US",  # Electronic Technology
+# "VRT.US",  # Electronic Technology
+# "TEL.US",  # Electronic Technology
+# "MSI.US",  # Electronic Technology
+# "STX.US",  # Electronic Technology
+# "WDC.US",  # Electronic Technology
+# "NXPI.US",  # Electronic Technology
+# "LHX.US",  # Electronic Technology
+# "MPWR.US",  # Electronic Technology
+# "AXON.US",  # Electronic Technology
+# "GRMN.US",  # Electronic Technology
+# "HEI.US",  # Electronic Technology
+# "KEYS.US",  # Electronic Technology
+# "UI.US",  # Electronic Technology
+# "PSTG.US",  # Electronic Technology
+# "MCHP.US",  # Electronic Technology
+# "SNDK.US",  # Electronic Technology
+# "TER.US",  # Electronic Technology
+# "HPE.US",  # Electronic Technology
+# "CIEN.US",  # Electronic Technology
+# "FSLR.US",  # Electronic Technology
+# "COHR.US",  # Electronic Technology
+# "BE.US",  # Electronic Technology
+# "ALAB.US",  # Electronic Technology
+# "TDY.US",  # Electronic Technology
+# "NTAP.US",  # Electronic Technology
+# "RKLB.US",  # Electronic Technology
+# "FLEX.US",  # Electronic Technology
+# "ON.US",  # Electronic Technology
+# "GFS.US",  # Electronic Technology
+# "SMCI.US",  # Electronic Technology
+# "FTAI.US",  # Electronic Technology
+# "NVT.US",  # Electronic Technology
+# "FTV.US",  # Electronic Technology
+# "IONQ.US",  # Electronic Technology
+# "BWXT.US",  # Electronic Technology
+# "FN.US",  # Electronic Technology
+# "TXT.US",  # Electronic Technology
+# "MTSI.US",  # Electronic Technology
+# "AVAV.US",  # Electronic Technology
+# "TSEM.US",  # Electronic Technology
+# "NXT.US",  # Electronic Technology
+# "ZBRA.US",  # Electronic Technology
+# "JOBY.US",  # Electronic Technology
+# "HII.US",  # Electronic Technology
+# "KTOS.US",  # Electronic Technology
+# "CR.US",  # Electronic Technology
+# "GOOG.US", # Technology Services
+# "MSFT.US", # Technology Services
+# "META.US", # Technology Services
+# "ORCL.US", # Technology Services
+# "NFLX.US", # Technology Services
+# "PLTR.US", # Technology Services
+# "IBM.US",  # Technology Services
+# "CRM.US",  # Technology Services
+# "APP.US",  # Technology Services
+# "INTU.US", # Technology Services
+# "NOW.US",  # Technology Services
+# "ACN.US",  # Technology Services
+# "ADBE.US", # Technology Services
+# "PANW.US", # Technology Services
+# "CRWD.US", # Technology Services
+# "SNOW.US", # Technology Services
+# "ADP.US",  # Technology Services
+# "NTES.US",  # Technology Services
+# "CDNS.US", # Technology Services
+# "SNPS.US", # Technology Services
+# "NET.US", # Technology Services
+# "ADSK.US", # Technology Services
+# "RBLX.US", # Technology Services
+# "FTNT.US", # Technology Services
+# "WDAY.US", # Technology Services
+# "DDOG.US", # Technology Services
+# "MSTR.US", # Technology Services
+# "EA.US", # Technology Services
+# "ROP.US", # Technology Services
+# "TTWO.US", # Technology Services
+# "RDDT.US", # Technology Services
+# "MSCI.US", # Technology Services
+# "BIDU.US", # Technology Services
+# "TEAM.US", # Technology Services
+# "PAYX.US", # Technology Services
+# "VEEV.US", # Technology Services
+# "ZS.US", # Technology Services
+# "CRWV.US", # Technology Services
+# "CTSH.US", # Technology Services
+# "FIS.US", # Technology Services
+# "MDB.US", # Technology Services
+# "CRDO.US", # Technology Services
+# "VRSK.US", # Technology Services
+# "FWONA.US", # Technology Services
+# "CSGP.US", # Technology Services
+# "BR.US", # Technology Services
+# "ZM.US", # Technology Services
+# "NBIS.US", # Technology Services
+# "LDOS.US", # Technology Services
+# "VRSN.US", # Technology Services
+# "HPQ.US", # Technology Services
+# "CYBR.US", # Technology Services
+# "IOT.US", # Technology Services
+# "PTC.US", # Technology Services
+# "SSNC.US", # Technology Services
+# "CHKP.US", # Technology Services
+# "TOST.US", # Technology Services
+# "TYL.US", # Technology Services
+# "TWLO.US", # Technology Services
+# "TRMB.US", # Technology Services
+# "HUBS.US", # Technology Services
+# "TTD.US", # Technology Services
+# "U.US", # Technology Services
+# "CDW.US", # Technology Services
+# "GWRE.US", # Technology Services
+# "CRCL.US", # Technology Services
+# "PINS.US", # Technology Services
+# "FIG.US", # Technology Services
+# "Z.US", # Technology Services
+# "GDDY.US", # Technology Services
+# "IT.US", # Technology Services
+# "GEN.US", # Technology Services
+# "J.US", # Technology Services
+# "ROKU.US", # Technology Services
+# "OKTA.US", # Technology Services
+# "FFIV.US", # Technology Services
+# "DOCU.US", # Technology Services
+# "RBRK.US", # Technology Services
+# "JPM.US",   # Finance
+# "V.US",     # Finance
+# "MA.US",    # Finance
+# "BAC.US",   # Finance
+# "MS.US",    # Finance
+# "WFC.US",   # Finance
+# "AXP.US",   # Finance
+# "GS.US",    # Finance
+# "C.US",     # Finance
+# "BX.US",    # Finance
+# "BLK.US",   # Finance
+# "SCHW.US",  # Finance
+# "COF.US",   # Finance
+# "WELL.US",   # Finance
+# "PGR.US",   # Finance
+# "PLD.US",   # Finance
+# "CB.US",   # Finance
+# "BN.US",  # Finance
+# "HOOD.US",   # Finance
+# "KKR.US",    # Finance
+# "IBKR.US",   # Finance
+# "CME.US",   # Finance
+# "MMC.US",  # Finance
+# "ICE.US",    # Finance
+# "NU.US",   # Finance
+# "BAM.US",  # Finance
+# "AMT.US",    # Finance
+# "BK.US",    # Finance
+# "USB.US",    # Finance
+# "APO.US",    # Finance
+# "PNC.US",    # Finance
+# "AON.US",    # Finance
+# "EQIX.US",    # Finance
+# "COIN.US",    # Finance
+# "TRV.US",    # Finance
+# "AJG.US",    # Finance
+# "SPG.US",    # Finance
+# "TFC.US",    # Finance
+# "AFL.US",    # Finance
+# "RKT.US",    # Finance
+# "ALL.US",    # Finance
+# "DLR.US",    # Finance
+# "O.US",    # Finance
+# "URI.US",    # Finance
+# "NDAQ.US",    # Finance
+# "MET.US",    # Finance
+# "PSA.US",    # Finance
+# "CBRE.US",    # Finance
+# "AMP.US",    # Finance
+# "AIG.US",    # Finance
+# "VTR.US",    # Finance
+# "CCI.US",    # Finance
+# "PRU.US",    # Finance
+# "HIG.US",    # Finance
+# "SOFI.US",    # Finance
+# "ARES.US",    # Finance
+# "ACGL.US",    # Finance
+# "STT.US",    # Finance
+# "RJF.US",    # Finance
+# "VICI.US",    # Finance
+# "WTW.US",    # Finance
+# "MTB.US",    # Finance
+# "FITB.US",    # Finance
+# "LPLA.US",    # Finance
+# "SYF.US",    # Finance
+# "EXR.US",    # Finance
+# "WRB.US",    # Finance
+# "BRO.US",    # Finance
+# "CBOE.US",    # Finance
+# "HBAN.US",    # Finance
+# "MKL.US",    # Finance
+# "CINF.US",    # Finance
+# "AVB.US",    # Finance
+# "TW.US",    # Finance
+# "NTRS.US",    # Finance
+# "AMZN.US",  # Retail Trade
+# "WMT.US",   # Retail Trade
+# "COST.US",  # Retail Trade
+# "BABA.US",  # Retail Trade
+# "HD.US",    # Retail Trade
+# "TJX.US",   # Retail Trade
+# "PDD.US",  # Retail Trade
+# "LOW.US",   # Retail Trade
+# "MELI.US",  # Retail Trade
+# "CVS.US",  # Retail Trade
+# "ORLY.US",  # Retail Trade
+# "CVNA.US",   # Retail Trade
+# "SE.US",   # Retail Trade
+# "AZO.US",  # Retail Trade
+# "ROST.US",  # Retail Trade
+# "CPNG.US",  # Retail Trade
+# "KR.US",  # Retail Trade
+# "TGT.US",  # Retail Trade
+# "JD.US",  # Retail Trade
+# "CPRT.US",  # Retail Trade
+# "EBAY.US",  # Retail Trade
+# "TSCO.US",  # Retail Trade
+# "ULTA.US",  # Retail Trade
+# "DG.US",  # Retail Trade
+# "TPR.US",  # Retail Trade
+# "DLTR.US",  # Retail Trade
+# "WSM.US",  # Retail Trade
+# "CASY.US",  # Retail Trade
+# "DKS.US",  # Retail Trade
+# "BBY.US",  # Retail Trade
+# "BURL.US",  # Retail Trade
+# "CHWY.US",  # Retail Trade
+# "W.US",  # Retail Trade
+# "BLDR.US",  # Retail Trade
+# "BJ.US",  # Retail Trade
+# "DDS.US",  # Retail Trade
+# "PAG.US",  # Retail Trade
+# "GME.US",  # Retail Trade
+# "GAP.US",  # Retail Trade
+# "VIPS.US",  # Retail Trade
+# "ACI.US",  # Retail Trade
+# "FIVE.US",  # Retail Trade
+# "SFM.US",  # Retail Trade
+# "LAD.US",  # Retail Trade
+# "AN.US",  # Retail Trade
+# "OLLI.US",  # Retail Trade
+# "MUSA.US",  # Retail Trade
+# "URBN.US",  # Retail Trade
+# "GLBE.US",  # Retail Trade
+# "FND.US",  # Retail Trade
+# "M.US",  # Retail Trade
+# "BOOT.US",  # Retail Trade
+# "KMX.US",  # Retail Trade
+# "ETSY.US",  # Retail Trade
+# "GPI.US",  # Retail Trade
+# "ANF.US",  # Retail Trade
+# "ABG.US",  # Retail Trade
+# "RUSHA.US",  # Retail Trade
+# "PSMT.US",  # Retail Trade
+# "TBBB.US",  # Retail Trade
+# "BBWI.US",  # Retail Trade
+# "PLBL.US",  # Retail Trade
+# "SIG.US",  # Retail Trade
+# "AEO.US",  # Retail Trade
+# "VSCO.US",  # Retail Trade
+# "ASO.US",  # Retail Trade
+# "AAP.US",  # Retail Trade
+# "CPRI.US",  # Retail Trade
+# "RH.US",  # Retail Trade
+# "BKE.US",  # Retail Trade
+# "KSS.US",  # Retail Trade
+# "WRBY.US",  # Retail Trade
+# "EYE.US",  # Retail Trade
+# "SAH.US",  # Retail Trade
+# "GRDN.US",  # Retail Trade
+# "RVLV.US",  # Retail Trade
+# "WMK.US",  # Retail Trade
+# "REAL.US",  # Retail Trade
+# "XLK.US",     
+# "XLE.US",     
+# "XLY.US", 
+# ]
+
+# all_tickers = default_tickers + [
+#     "CRM.US",     
+#     "ORCL.US",    
+#     "INTC.US",    
+#     "AMD.US",     
+#     "NFLX.US",
+#     "BAC.US",     
+#     "WFC.US",     
+#     "GS.US",      
+#     "MS.US",      
+#     "UNH.US",
+#     "PFE.US",     
+#     "TMO.US",
+#     "KO.US",      
+#     "WMT.US",     
+#     "DIS.US",     
+#     "NKE.US",     
+#     "BA.US",      
+#     "CAT.US",     
+#     "GE.US",      
+# ]
 
 raw_data_path = Path(__file__).parent.parent / "data" / "raw"
 
---
+++ data/dataset_builder.py
@@ -0,0 +1,140 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+from data_download import default_tickers
+from feature_engineering import load_csv_ticker, merge_and_align_ticker_data, add_features
+
+
+def build_base_dataset():
+    output_path = Path(__file__).parent / "processed"
+    output_path.mkdir(exist_ok=True)
+    
+    tickers = [t.replace('.US', '') for t in default_tickers]
+    ticker_data = {ticker: load_csv_ticker(ticker) for ticker in tickers}
+    aligned_data = merge_and_align_ticker_data(ticker_data)
+
+    aligned_path = output_path / "aligned.csv"
+    aligned_data.to_csv(aligned_path)
+    
+    enhanced_data = add_features(aligned_data)
+    features_path = output_path / "features.csv"
+    enhanced_data.to_csv(features_path)
+
+    return enhanced_data
+
+
+def build_multi_dataset(df, window=60):
+    if "date" in df.columns:
+        df = df.set_index("date")
+    df = df.sort_index()
+
+    ticker_feature_map = {}
+    for col in df.columns:
+        if "_" not in col:
+            continue
+        ticker_symbol, feature_name = col.split("_", 1)
+        if ticker_symbol not in ticker_feature_map:
+            ticker_feature_map[ticker_symbol] = {}
+        ticker_feature_map[ticker_symbol][feature_name] = col
+
+    all_tickers = sorted(ticker_feature_map.keys())
+
+    excluded_features = {"ret_1d", "open", "high", "low"}
+    common_features = None
+    
+    for ticker in all_tickers:
+        ticker_features = {f for f in ticker_feature_map[ticker].keys() 
+                          if f not in excluded_features}
+        if common_features is None:
+            common_features = ticker_features
+        else:
+            common_features = common_features & ticker_features
+    
+    common_features = sorted(common_features)
+
+    model_ready_tickers = []
+    for ticker in all_tickers:
+        has_all_features = all(feat in ticker_feature_map[ticker] 
+                              for feat in common_features)
+        has_returns = "ret_1d" in ticker_feature_map[ticker]
+        
+        if has_all_features and has_returns:
+            model_ready_tickers.append(ticker)
+
+    normalized_df = df.copy()
+    epsilon = 1e-8
+    
+    for ticker in model_ready_tickers:
+        feature_columns = [ticker_feature_map[ticker][feat] 
+                          for feat in common_features]
+        ticker_subset = normalized_df[feature_columns]
+        normalized_df[feature_columns] = (
+            (ticker_subset - ticker_subset.mean()) / 
+            (ticker_subset.std(ddof=0) + epsilon)
+        )
+    
+
+    return_columns = [ticker_feature_map[ticker]["ret_1d"] 
+                     for ticker in model_ready_tickers]
+    return_clip_lower = -0.20
+    return_clip_upper = 0.20
+    y_returns = df[return_columns].clip(
+        lower=return_clip_lower, 
+        upper=return_clip_upper
+    ).values
+
+    X_list = []
+    for ticker in model_ready_tickers:
+        ticker_columns = [ticker_feature_map[ticker][feat] 
+                         for feat in common_features]
+        ticker_features = normalized_df[ticker_columns].values
+        ticker_features_expanded = ticker_features[:, np.newaxis, :]
+        X_list.append(ticker_features_expanded)
+
+    X_full = np.concatenate(X_list, axis=1)
+
+    X_windows = []
+    y_windows = []
+    total_timesteps = X_full.shape[0]
+    num_windows = total_timesteps - window
+
+    for i in range(num_windows):
+        window_start = i
+        window_end = i + window
+        X_window = X_full[window_start:window_end]
+        X_window_transposed = np.transpose(X_window, (1, 0, 2))
+        y_index = window_end
+        y_value = y_returns[y_index]
+        X_windows.append(X_window_transposed)
+        y_windows.append(y_value)
+
+
+    X_final = np.stack(X_windows).astype(np.float32)
+    y_final = np.stack(y_windows).astype(np.float32)
+    return X_final, y_final, model_ready_tickers, common_features
+
+def save_multi_dataset():
+    output_path = Path(__file__).parent / "processed"
+    features_path = output_path / "features.csv"
+    
+    df = pd.read_csv(features_path, parse_dates=["date"])
+    X_final, y_final, model_ready_tickers, common_features = build_multi_dataset(df, window=60)
+    
+    np.save(output_path / "multi_X.npy", X_final)
+    np.save(output_path / "multi_y.npy", y_final)
+    np.save(output_path / "multi_tickers.npy", np.array(model_ready_tickers))
+    
+    return X_final, y_final, model_ready_tickers, common_features
+
+if __name__ == "__main__":
+    print("Installing base dataset...")
+    df = build_base_dataset()
+    print("Base dataset built.")
+
+    print("Building multi-ticker dataset...")
+    X, y, tickers, features = save_multi_dataset()
+    print("Done!")
+    print("Shapes:", X.shape, y.shape)
